a441a70ee6bf336c090476a2e3496f4aae3d848b
nicholasscabral <nicholasscabral@gmail.com>
2021-09-09 02:25:45 -0300

    importing public folder

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import "./database";
 import { routes } from "./routes";
+import path from "path";
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(express.json());
 
