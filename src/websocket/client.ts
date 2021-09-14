e5097bf8e5c060eb2ea6a44f1c9fc49c32685ec4
nicholasscabral <nicholasscabral@gmail.com>
2021-09-14 20:40:06 -0300

    connecting client

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,8 +1,30 @@
 import { io } from "../http";
+import { ConnectionsService } from "../services/ConnectionsService";
+import { UsersService } from "../services/UsersService";
 
 io.on("connect", (socket) => {
-  socket.on("client_first_access", (params) => {
-    console.log(params);
+  const connectionsService = new ConnectionsService();
+  const usersService = new UsersService();
+
+  socket.on("client_first_access", async (params) => {
+    const socket_id = socket.id;
+    const { text, email } = params;
+
+    const userExists = await usersService.findByEmail(email);
+
+    if (!userExists) {
+      const user = await usersService.create(email);
+
+      await connectionsService.create({
+        socket_id,
+        user_id: user.id,
+      });
+    } else {
+      await connectionsService.create({
+        socket_id,
+        user_id: userExists.id,
+      });
+    }
 
     // salvar a conexão com o socket_id, user_id
   });
