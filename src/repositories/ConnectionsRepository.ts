276fc789b76495f9ec41619d55471b9f378f01d2
nicholasscabral <nicholasscabral@gmail.com>
2021-09-14 19:15:58 -0300

    connections repository and services

---
+++ src/repositories/ConnectionsRepository.ts
@@ -0,0 +1,6 @@
+import { Repository } from "typeorm";
+import { Connection } from "../entities/Connection";
+
+class ConnectionsRepository extends Repository<Connection> {}
+
+export { ConnectionsRepository };
